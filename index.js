789e5214eb6b987adccae4b7fad49447142d9ebb
cward22 <h2hworldwide@gmail.com>
2020-04-01 07:24:52 -0400

    removed .get '/admin' from index.js

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/admin', (req, res) => res.render('pages/db'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 //adfbsdsfgndfgn
